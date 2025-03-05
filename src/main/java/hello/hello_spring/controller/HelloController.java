1e594135c200cec380325f5ab97e82d4b287fdd5
hyunjun.jin <hyunjun6210@naver.com>
2025-03-05 19:06:47 +0900

    Create domain and repository

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,7 +18,32 @@ public class HelloController {
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) { //require == false로 하면 default 값이 true 라 ?name="VALUE" 로 값 넘겨줌
         model.addAttribute("name", name);
-        
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //데이터 그대로 내려주는 방식
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
